f89178e974385e51758e098b059da1a3a12da02a
Ellis Patrick <ellis.patrick@sydney.edu.au>
2023-07-31 17:52:58 -0400

    Just a periodic save [skip ci]

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' MIBI-TOF Breast cancer intensities
+#'
+#' @description
+#' This is a filtered MIBI-TOF dataset of breast cancer from the
+#' Keren et al 2018 dataset.
+#'
+#'
+#' @usage data(kerenSPE)
+#'
+#' @format kerenSPE a SpatialExperiment object
+#' @aliases
+#' kerenSCE
+#'
+#' @references Keren, L., Bosse, M., Marquez, D., Angoshtari, R., Jain,
+#' S., Varma, S., Yang, S. R., Kurian, A., Van Valen, D., West, R., Bendall,
+#' S. C., & Angelo, M. (2018). A Structured Tumor-Immune Microenvironment in
+#' Triple Negative Breast Cancer Revealed by Multiplexed Ion Beam Imaging.
+#' Cell, 174(6), 1373-1387.e1319. ([DOI](https://doi.org/10.1016/j.cell.2018.08.039))
+#'
+#'
+"kerenSPE"
+
